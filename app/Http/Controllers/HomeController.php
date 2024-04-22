b1010aa74ebda9675304931b2874d0257037701b
antoine-codeur <antoinejf.denoue@gmail.com>
2024-04-22 13:38:12 +0200

    mise en page home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,7 +12,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $latestPosts = Post::latest()->take(5)->get();
+        $latestPosts = Post::latest()->take(9)->get();
         return view('home', compact('latestPosts'));
     }
 }
